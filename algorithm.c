b5aaa55e1693e5b87cfe14c38fcfa67bbf8ef2de
Luiz Felipe Kim <luizfke@gmail.com>
2018-01-14 11:07:40 -0200

    arquivo modificado

--- algorithm.c
+++ algorithm.c
@@ -17,12 +17,27 @@
 #include <math.h>
 #include "driverlib/algorithm.h"
 
+// Retornar o maior valor positivo de um vetor
+int _max(int n, int vector[]) {
+	int i;
+	int maior = 0;
+
+	for (i=0; i<n; i++) { // Roda o vetor
+		if (vector[i] > maior) {		// Se for maior que o maior armazenado, troca
+			maior = vector[i];
+		}
+	}
+
+	return maior;						// Retorna o maior valor
+}
+
+/*
 // Retornar o maior valor positivo de um vetor de float
-float _maxFloat(float vector[]) {
+float _maxFloat(int n, float vector[]) {
 	int i;
 	float maior = 0;
 
-	for (i=0; i<2*DELAY_MAX + 1; i++) { // Roda o vetor
+	for (i=0; i<n; i++) { // Roda o vetor
 		if (vector[i] > maior) {		// Se for maior que o maior armazenado, troca
 			maior = vector[i];
 		}
@@ -36,23 +51,23 @@ void _normalizar() {
 	uint32_t maxValue = 0;
 	int i;
 
-	maxValue = _maxFloat(bufferDatabase);						// Obter o maior valor do vetor DB
+	maxValue = _maxFloat(NUM_AMOSTRAS + 2*DELAY_MAX, bufferDatabase);		// Obter o maior valor do vetor DB
 
 	for (i=0; i<NUM_AMOSTRAS + 2*DELAY_MAX; i++) {				// Roda os valores
 		bufferDatabase[i] = (bufferDatabase[i])/(maxValue);		// Realiza a divisão
 	}
 
-	maxValue = _maxFloat(bufferConversao);						// Obter o maior valor do vetor AQ
+	maxValue = _maxFloat(NUM_AMOSTRAS + 2*DELAY_MAX, bufferConversao);	// Obter o maior valor do vetor AQ
 
 	for (i=0; i<NUM_AMOSTRAS + 2*DELAY_MAX; i++) {				// Roda os valores
 		bufferConversao[i] = (bufferConversao[i])/(maxValue);	// Realiza a divisão
 	}
 
 	return;
-}
+} */
 
 // Retorna o valor central da autocorrelação de um vetor (Rxx[0])
-float autoCorrelate(float signal[]) {
+int autoCorrelate(int signal[]) {
 	int n;
 	rAuto = 0;
 
@@ -64,7 +79,7 @@ float autoCorrelate(float signal[]) {
 }
 
 // Retorna o maior valor de crosscorrelação entre dois vetores (max(Rxy[k]))
-float crossCorrelate(float signal1[], float signal2[]) {
+int crossCorrelate(int signal1[], int signal2[]) {
 	int n, k;
 
 	for (k = -DELAY_MAX; k <= DELAY_MAX; k++) {						// Para os atrasos relevantes
@@ -73,15 +88,17 @@ float crossCorrelate(float signal1[], float signal2[]) {
 		}
 	}
 
-	return _maxFloat(rCross);										// Retorna o maior valor
+	return _max(2*DELAY_MAX + 1, rCross);
+	//return _maxFloat(2*DELAY_MAX + 1, rCross);						// Retorna o maior valor
 }
 
 // Validar se o sinal AQ é parecido com o DB, considerando a tolerância
 bool validate() {
 	float coefCorrelacao;
-	float auto1, auto2, cross12;
+	//float
+	int auto1, auto2, cross12;
 
-	_normalizar();													// Escala os vetores
+	//_normalizar();													// Escala os vetores
 
 	auto1 = autoCorrelate(bufferDatabase);							// Calcula Autocorrelação do sinal DB
 	auto2 = autoCorrelate(bufferConversao);							// Calcula Autocorrelação do sinal AQ
