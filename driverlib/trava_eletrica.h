c5601d3bef592b95827b194b3c5fb5a61b4922ce
tiagofaxina <tiagofaxina@alunos.utfpr.edu.br>
2018-01-31 14:52:21 -0200

    Update trava_eletrica.h

--- driverlib/trava_eletrica.h
+++ driverlib/trava_eletrica.h
@@ -1,9 +1,21 @@
-/*
- * trava_eletrica.h
- *
- *  Created on: 1 de set de 2017
- *      Author: thfax
- */
+/* ************************************************************** *
+ * UTFPR - Universidade Tecnologica Federal do Paraná
+ * Engenharia Eletrônica
+ * Trabalho de Conclusão de Curso
+ * ************************************************************** *
+ * Sistema de Segurança baseado em Reconhecimento de Senha Falada
+ * ************************************************************** *
+ * Equipe:
+ * Luiz Felipe Kim Evaristo
+ * Tauan Marinho
+ * Tiago Henrique Faxina
+ * Tiago Mariani Palte
+ * ************************************************************** *
+ * Header do Controlador da Trava Elétrica
+ * v1.0
+ * 2017
+ * ************************************************************** */
+
 
 #ifndef TRAVA_ELETRICA_H_
 #define TRAVA_ELETRICA_H_
