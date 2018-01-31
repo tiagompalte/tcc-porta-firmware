d953c02df8ef590a6a784d4b93d49a6c9917b18b
tiagofaxina <tiagofaxina@alunos.utfpr.edu.br>
2018-01-31 14:51:47 -0200

    Update buzzer.h

--- driverlib/buzzer.h
+++ driverlib/buzzer.h
@@ -1,9 +1,21 @@
-/*
- * buzzer.h
- *
- *  Created on: 3 de set de 2017
- *      Author: thfax
- */
+/* ************************************************************** *
+ * UTFPR - Universidade Tecnologica Federal do Paraná
+ * Engenharia Eletrônica
+ * Trabalho de Conclusão de Curso
+ * ************************************************************** *
+ * Sistema de Segurança baseado em Reconhecimento de Senha Falada
+ * ************************************************************** *
+ * Equipe:
+ * Luiz Felipe Kim Evaristo
+ * Tauan Marinho
+ * Tiago Henrique Faxina
+ * Tiago Mariani Palte
+ * ************************************************************** *
+ * Header do Controlador do Buzzer
+ * v1.0
+ * 2017
+ * ************************************************************** */
+
 
 #ifndef BUZZER_H_
 #define BUZZER_H_
