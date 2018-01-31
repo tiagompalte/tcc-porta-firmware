3fd2f5da891e725046ff1671dc96a1c39b2e213e
tiagofaxina <tiagofaxina@alunos.utfpr.edu.br>
2018-01-31 14:51:26 -0200

    Update MyString.h

--- driverlib/MyString.h
+++ driverlib/MyString.h
@@ -1,9 +1,21 @@
-/*
- * MyString.h
- *
- *  Created on: 5 de nov de 2017
- *      Author: thfax
- */
+/* ************************************************************** *
+ * UTFPR - Universidade Tecnologica Federal do Paraná
+ * Engenharia Eletrônica
+ * Trabalho de Conclusão de Curso
+ * ************************************************************** *
+ * Sistema de Segurança baseado em Reconhecimento de Senha Falada
+ * ************************************************************** *
+ * Equipe:
+ * Luiz Felipe Kim Evaristo
+ * Tauan Marinho
+ * Tiago Henrique Faxina
+ * Tiago Mariani Palte
+ * ************************************************************** *
+ * Realiza Operações com Strings
+ * v1.0
+ * 2017
+ * ************************************************************** */
+
 
 #ifndef MYSTRING_H_
 #define MYSTRING_H_
