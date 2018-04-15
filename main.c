664d87f48ea88c0b251b41d856d2fc0e7511f098
Tauan Marinho <tauamarinho@gmail.com>
2018-04-15 17:04:15 -0300

    Formatação e ajuste de status após receber o resultadoda escolha voz/senha

--- main.c
+++ main.c
@@ -21,7 +21,6 @@
 #include <math.h>
 #include <string.h>
 
-
 #include "inc/hw_types.h"
 #include "inc/hw_ints.h"
 #include "inc/hw_memmap.h"
@@ -100,14 +99,18 @@ UserStatus uStatus = EntryNotAllowed;
 /*
  * ISRs
  */
-void ISR_Timer1A() {
+void ISR_Timer1A()
+{
     MAP_TimerIntClear(TIMER1_BASE, TIMER_TIMA_TIMEOUT);
 
-    if (led3s == 0) {
+    if (led3s == 0)
+    {
         MAP_GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
         MAP_GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_2, GPIO_PIN_2);
         led3s = 1;
-    } else {
+    }
+    else
+    {
         led3s = 0;
         MAP_GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0);
         MAP_GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_2, 0);
@@ -116,21 +119,25 @@ void ISR_Timer1A() {
     return;
 }
 
-void ISR_ADC0() {
+void ISR_ADC0()
+{
     MAP_ADCIntClear(ADC0_BASE, 3);
     MAP_ADCSequenceDataGet(ADC0_BASE, 3, bufferCapture);
 
-    if (indiceAmostra <= NUM_AMOSTRAS - 1) {
-      //bufferConversao[indiceAmostra] = scaleSample(bufferCapture[0]);
-      bufferConversao[indiceAmostra] = bufferCapture[0];
-      indiceAmostra++;
-    } else {
-      MAP_TimerDisable(TIMER0_BASE, TIMER_A);
-      MAP_ADCSequenceDisable(ADC0_BASE, 3);
-      MAP_ADCIntDisable(ADC0_BASE, 3);
-      MAP_IntDisable(INT_ADC0SS3);
-      indiceAmostra = 0;
-      conversionEnd = 1;
+    if (indiceAmostra <= NUM_AMOSTRAS - 1)
+    {
+        //bufferConversao[indiceAmostra] = scaleSample(bufferCapture[0]);
+        bufferConversao[indiceAmostra] = bufferCapture[0];
+        indiceAmostra++;
+    }
+    else
+    {
+        MAP_TimerDisable(TIMER0_BASE, TIMER_A);
+        MAP_ADCSequenceDisable(ADC0_BASE, 3);
+        MAP_ADCIntDisable(ADC0_BASE, 3);
+        MAP_IntDisable(INT_ADC0SS3);
+        indiceAmostra = 0;
+        conversionEnd = 1;
     }
 
     return;
@@ -139,11 +146,15 @@ void ISR_ADC0() {
 /*
  * Funï¿½ï¿½es Auxiliares
  */
-void systemInit() {
-	g_ui32SysClock = MAP_SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480), 120000000);
+void systemInit()
+{
+    g_ui32SysClock = MAP_SysCtlClockFreqSet(
+            (SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL
+                    | SYSCTL_CFG_VCO_480),
+            120000000);
 
-	MAP_SysCtlPeripheralPowerOn(SYSCTL_PERIPH_GPIOB);
-	MAP_SysCtlPeripheralPowerOn(SYSCTL_PERIPH_GPIOD);
+    MAP_SysCtlPeripheralPowerOn(SYSCTL_PERIPH_GPIOB);
+    MAP_SysCtlPeripheralPowerOn(SYSCTL_PERIPH_GPIOD);
     MAP_SysCtlPeripheralPowerOn(SYSCTL_PERIPH_GPIOE);
     MAP_SysCtlPeripheralPowerOn(SYSCTL_PERIPH_GPIOG);
     MAP_SysCtlPeripheralPowerOn(SYSCTL_PERIPH_GPIOK);
@@ -157,74 +168,95 @@ void systemInit() {
     MAP_SysCtlPeripheralPowerOn(SYSCTL_PERIPH_SSI2);
 }
 
-void GPIOInit() {
+void GPIOInit()
+{
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI2);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_SSI2));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_SSI2))
+        ;
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB))
+        ;
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOC));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOC))
+        ;
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD))
+        ;
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE))
+        ;
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
+        ;
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOG);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOG));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOG))
+        ;
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ))
+        ;
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOK);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOK));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOK))
+        ;
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOL);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOL));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOL))
+        ;
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOM);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOM));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOM))
+        ;
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
 
-    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)) {
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))
+    {
 
     }
     MAP_GPIOPinConfigure(GPIO_PD3_SSI2CLK);
     MAP_GPIOPinConfigure(GPIO_PD0_SSI2XDAT1);
     MAP_GPIOPinConfigure(GPIO_PD1_SSI2XDAT0); //MOSI
 
-    MAP_GPIOPinTypeSSI(GPIO_PORTD_BASE, GPIO_PIN_1|GPIO_PIN_0|GPIO_PIN_3);
-    
-    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE,GPIO_PIN_3|GPIO_PIN_2);
-    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTC_BASE,GPIO_PIN_4);
-    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTG_BASE,GPIO_PIN_0);
-    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE,GPIO_PIN_7|GPIO_PIN_2);
-    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTM_BASE, GPIO_PIN_2|GPIO_PIN_1|GPIO_PIN_0);
-    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTK_BASE, GPIO_PIN_7|GPIO_PIN_6|GPIO_PIN_5|GPIO_PIN_4|GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1|GPIO_PIN_0);
-    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
-    
-    MAP_GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1|GPIO_PIN_0);
-    MAP_GPIOPinTypeGPIOInput(GPIO_PORTL_BASE, GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1|GPIO_PIN_0);
-    MAP_GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_1|GPIO_PIN_0);
-    
+    MAP_GPIOPinTypeSSI(GPIO_PORTD_BASE, GPIO_PIN_1 | GPIO_PIN_0 | GPIO_PIN_3);
+
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_3 | GPIO_PIN_2);
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTC_BASE, GPIO_PIN_4);
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTG_BASE, GPIO_PIN_0);
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_7 | GPIO_PIN_2);
+    MAP_GPIOPinTypeGPIOOutput(GPIO_PORTM_BASE,
+                              GPIO_PIN_2 | GPIO_PIN_1 | GPIO_PIN_0);
+    MAP_GPIOPinTypeGPIOOutput(
+            GPIO_PORTK_BASE,
+            GPIO_PIN_7 | GPIO_PIN_6 | GPIO_PIN_5 | GPIO_PIN_4 | GPIO_PIN_3
+                    | GPIO_PIN_2 | GPIO_PIN_1 | GPIO_PIN_0);
+    MAP_GPIOPinTypeGPIOOutput(
+            GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+
+    MAP_GPIOPinTypeGPIOInput(GPIO_PORTE_BASE,
+                             GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1 | GPIO_PIN_0);
+    MAP_GPIOPinTypeGPIOInput(GPIO_PORTL_BASE,
+                             GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1 | GPIO_PIN_0);
+    MAP_GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_1 | GPIO_PIN_0);
+
     MAP_GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_5);
 }
 
 void SSIInit()
 {
     uint32_t initialData = 0;
-    MAP_SSIIntClear(SSI2_BASE,SSI_TXEOT);
+    MAP_SSIIntClear(SSI2_BASE, SSI_TXEOT);
     MAP_SSIClockSourceSet(SSI2_BASE, SSI_CLOCK_SYSTEM);
     MAP_SSIAdvModeSet(SSI2_BASE, SSI_ADV_MODE_LEGACY);
     MAP_SSIConfigSetExpClk(SSI2_BASE, g_ui32SysClock, SSI_FRF_MOTO_MODE_0,
-                               SSI_MODE_MASTER, 1000000, 16);
+    SSI_MODE_MASTER,
+                           1000000, 16);
     MAP_SSIEnable(SSI2_BASE);
 
     //clear out any initial data that might be present in the RX FIFO
-    while(MAP_SSIDataGetNonBlocking(SSI2_BASE, &initialData))
+    while (MAP_SSIDataGetNonBlocking(SSI2_BASE, &initialData))
     {
 
     }
 }
 
-
-void interruptInit() {
+void interruptInit()
+{
     MAP_ADCIntDisable(ADC0_BASE, 3);
     MAP_IntDisable(INT_ADC0SS3);
     MAP_ADCIntClear(ADC0_BASE, 3);
@@ -238,11 +270,11 @@ void interruptInit() {
     TimerIntRegister(TIMER4_BASE, TIMER_A, &BuzzerIntHandler);
     MAP_TimerIntEnable(TIMER4_BASE, TIMER_TIMA_TIMEOUT);
     MAP_IntEnable(INT_TIMER4A);
-    
+
     TimerIntRegister(TIMER3_BASE, TIMER_A, &LCDIntHandler);
     MAP_TimerIntEnable(TIMER3_BASE, TIMER_TIMA_TIMEOUT);
     MAP_IntEnable(INT_TIMER3A);
-    
+
     TimerIntRegister(TIMER5_BASE, TIMER_A, &PasswordIntHandler);
     MAP_TimerIntEnable(TIMER5_BASE, TIMER_TIMA_TIMEOUT);
     MAP_IntEnable(INT_TIMER5A);
@@ -250,10 +282,12 @@ void interruptInit() {
     MAP_IntMasterEnable();
 }
 
-void timerInit() {
+void timerInit()
+{
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
 
-    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)) {
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0))
+    {
 
     }
 
@@ -266,7 +300,8 @@ void timerInit() {
 
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
 
-    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER1)) {
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER1))
+    {
 
     }
 
@@ -275,45 +310,53 @@ void timerInit() {
     MAP_TimerClockSourceSet(TIMER1_BASE, TIMER_CLOCK_PIOSC);
     MAP_TimerLoadSet(TIMER1_BASE, TIMER_A, 48484848);
 
-/*    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER2)) {
+    /*    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER2)) {
 
-    }
+     }
 
-    MAP_TimerConfigure(TIMER2_BASE, TIMER_CFG_PERIODIC_UP);
-    TimerUpdateMode(TIMER2_BASE, TIMER_BOTH, TIMER_UP_LOAD_IMMEDIATE);
-    MAP_TimerClockSourceSet(TIMER2_BASE, TIMER_CLOCK_PIOSC);
-    MAP_TimerLoadSet(TIMER2_BASE, TIMER_A, 145454544);*/
-    
+     MAP_TimerConfigure(TIMER2_BASE, TIMER_CFG_PERIODIC_UP);
+     TimerUpdateMode(TIMER2_BASE, TIMER_BOTH, TIMER_UP_LOAD_IMMEDIATE);
+     MAP_TimerClockSourceSet(TIMER2_BASE, TIMER_CLOCK_PIOSC);
+     MAP_TimerLoadSet(TIMER2_BASE, TIMER_A, 145454544);*/
+
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER4);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER4));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER4))
+        ;
     MAP_TimerConfigure(TIMER4_BASE, TIMER_CFG_A_ONE_SHOT);
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER3);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER3));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER3))
+        ;
     MAP_TimerConfigure(TIMER3_BASE, TIMER_CFG_A_ONE_SHOT);
     MAP_TimerLoadSet(TIMER3_BASE, TIMER_A, 3 * g_ui32SysClock);
 
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER5);
-    while(!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER5));
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER5))
+        ;
 
     MAP_TimerConfigure(TIMER5_BASE, TIMER_CFG_A_ONE_SHOT);
     MAP_TimerLoadSet(TIMER5_BASE, TIMER_A, 10 * ui32SysClock);
 
 }
 
-void ADCInit() {
+void ADCInit()
+{
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
     MAP_SysCtlPeripheralReset(SYSCTL_PERIPH_ADC0);
 
-    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0)) {
+    while (!MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0))
+    {
 
     }
 
-    ADCClockConfigSet(ADC0_BASE, (ADC_CLOCK_SRC_PIOSC | ADC_CLOCK_RATE_FULL), 1);
+    ADCClockConfigSet(ADC0_BASE, (ADC_CLOCK_SRC_PIOSC | ADC_CLOCK_RATE_FULL),
+                      1);
     MAP_ADCPhaseDelaySet(ADC0_BASE, ADC_PHASE_0);
     MAP_ADCReferenceSet(ADC0_BASE, ADC_REF_INT);
     MAP_ADCSequenceDisable(ADC0_BASE, 3);
     MAP_ADCSequenceConfigure(ADC0_BASE, 3, ADC_TRIGGER_TIMER, 0);
-    MAP_ADCSequenceStepConfigure(ADC0_BASE, 3, 0, (ADC_CTL_IE | ADC_CTL_SHOLD_8 | ADC_CTL_CH8 | ADC_CTL_END));
+    MAP_ADCSequenceStepConfigure(
+            ADC0_BASE, 3, 0,
+            (ADC_CTL_IE | ADC_CTL_SHOLD_8 | ADC_CTL_CH8 | ADC_CTL_END));
     MAP_ADCSequenceEnable(ADC0_BASE, 3);
 }
 
@@ -324,9 +367,11 @@ __error__(char *pcFilename, uint32_t ui32Line)
 }
 #endif
 
-void SysTickIntHandler(void) {
+void SysTickIntHandler(void)
+{
     EthClientTick(SYSTEM_TICK_MS);
-    if(g_ui32Delay != 0) {
+    if (g_ui32Delay != 0)
+    {
         g_ui32Delay--;
     }
 }
@@ -349,17 +394,6 @@ void SysTickIntHandler(void) {
 void principalLoop()
 {
     HardwareLoop(); //adquire o rfid
-    //strcpy(g_psUserInfo.sReport.rfid, "12345678");
-    // Vai ser voz ou senha?
-    // status = hardwareVoiceKey();
-    //if (status == -1)
-    //{
-    //Não digitou nada
-    //}
-    //else
-    // {
-    //Leitura da voz ou do teclado
-    //  HardwarePassWordControl();//Aparece no display
 
     if (CommunicationConnecting() == ERROR)
     {
@@ -391,11 +425,20 @@ void principalLoop()
     //Leitura do teclado
 
     int status = NONE;
+    status = hardwareVoiceKey();
     printf("\n %c", status);
-    //status = hardwareVoiceKey();
-    status = VOICE;
-    HardwarePassWordControl(status);
-    if (CommunicationSending(status) == ERROR){
+    //status = VOICE;
+    if (status == ERROR)
+    {
+        principalLoop();    //Opção não digitada
+    }
+    else
+    {
+        HardwarePassWordControl(status);
+    }
+
+    if (CommunicationSending(status) == ERROR)
+    {
         //strcpy(g_psUserInfo.sReport.log, str_UserNotRegistered);
         uStatus = EntryNotAllowed;
         HardwareControl(uStatus);
@@ -412,12 +455,7 @@ void principalLoop()
     }
 }
 
-
-
-
-
-int
-main(void)
+int main(void)
 {
     led3s = 0;
     ledConv = 0;
@@ -428,43 +466,43 @@ main(void)
     systemInit();
     GPIOInit();
     SSIInit();
-    // Inicializar perifÃˆricos
+    // Inicializar perifericos
     ADCInit();
     timerInit();
     interruptInit();
     HardwareInit();
-	PinoutSet(true, false);
+    PinoutSet(true, false);
 
-	SysTickPeriodSet((g_ui32SysClock / 1000) * SYSTEM_TICK_MMS);
-	SysTickEnable();
-	SysTickIntEnable();
-	FlashPBInit(FLASH_PB_START, FLASH_PB_END, 256);
+    SysTickPeriodSet((g_ui32SysClock / 1000) * SYSTEM_TICK_MMS);
+    SysTickEnable();
+    SysTickIntEnable();
+    FlashPBInit(FLASH_PB_START, FLASH_PB_END, 256);
 
-	ResetUser();
+    ResetUser();
 
-	UpdateIPAddress(g_pcIPAddr, 0);
+    UpdateIPAddress(g_pcIPAddr, 0);
 
-	IntPriorityGroupingSet(4);
-	IntPrioritySet(INT_EMAC0, ETHERNET_INT_PRIORITY);
-	IntPrioritySet(FAULT_SYSTICK, SYSTICK_INT_PRIORITY);
+    IntPriorityGroupingSet(4);
+    IntPrioritySet(INT_EMAC0, ETHERNET_INT_PRIORITY);
+    IntPrioritySet(FAULT_SYSTICK, SYSTICK_INT_PRIORITY);
 
-	EthClientProxySet(0);
-	EthClientInit(EnetEvents);
+    EthClientProxySet(0);
+    EthClientInit(EnetEvents);
 
-	g_ui32IPaddr = EthClientAddrGet();
+    g_ui32IPaddr = EthClientAddrGet();
 
-	strcpy(g_psUserInfo.sReport.idBoard, IDBOARD);
-	strcpy(g_psUserInfo.sReport.keyBoard, KEYBOARD);
-	//strcpy(g_psUserInfo.sReport.rfid, "C9 42 6A 7B");
+    strcpy(g_psUserInfo.sReport.idBoard, IDBOARD);
+    strcpy(g_psUserInfo.sReport.keyBoard, KEYBOARD);
+    //strcpy(g_psUserInfo.sReport.rfid, "C9 42 6A 7B");
 
-
-	strcpy(g_psUserInfo.sReport.userKey, "1234");
+    strcpy(g_psUserInfo.sReport.userKey, "1234");
 
     //Ligando o equipamento, inicia o display
 
     LCDInicio();
-    while (1) {
-       principalLoop();
+    while (1)
+    {
+        principalLoop();
 
     }
 
